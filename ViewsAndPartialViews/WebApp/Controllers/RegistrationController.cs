8cc317560473feb17c2c2a256190ffe8f1a5f166
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2023-05-08 00:52:28 +0200

    Update to github

---
+++ ViewsAndPartialViews/WebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models.Services;
+using WebApp.Models.ViewModels;
+
+namespace WebApp.Controllers;
+
+public class RegistrationController : Controller
+{
+    private readonly AuthService _auth;
+
+    public RegistrationController(AuthService auth)
+    {
+        _auth = auth;
+    }
+
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Registration";
+        return View();
+    }
+
+    [HttpPost]
+    public async Task <IActionResult> Index(UserRegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            if (await _auth.RegAsync(model))
+
+            return RedirectToAction("Index", "Login");
+
+            ModelState.AddModelError("", "A user with the same email already exists");
+        }
+
+        return View(model);
+    }
+}
